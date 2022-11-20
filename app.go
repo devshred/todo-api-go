4b9e8c89597dd3bece626f3a6e1b1f29d81a42d0
Johannes Schmidt <devshred@tigerflanke.de>
2022-11-20 12:38:07 +0100

    measure startup time

--- app.go
+++ app.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 )
+
+var start = time.Now()
 
 func init() {
 	LoadEnvVariables()
@@ -23,6 +28,10 @@ func main() {
 		todoRoutes.DELETE("/:id", DeleteTodo)
 		todoRoutes.GET("/:id", GetTodo)
 	}
+
+	log.WithFields(log.Fields{
+		"time_sec": time.Since(start).Seconds(),
+	}).Info("Started Application")
 
 	router.Run()
 }
